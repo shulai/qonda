07c928ea3b862a691216b7f7b99bcf9918ef33d8
Julio Gazquez <julio@mebamutual.com.ar>
2012-12-16 16:10:14 -0300

    Cambio acciones de EditableView al keyPressEvent

---
+++ widgets/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 *-*
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import Qt
+
+
+class EditableView(object):
+
+    def keyReleaseEvent(self, event):
+        # TODO: Implement
+        key = event.key()
+        mod = event.modifiers()
+        current_row = self.currentIndex().row()
+        root = self.rootIndex()
+        row_count = self.model.rowCount(root)
+        if (mod == Qt.NoModifier and key == Qt.Key_Down and
+            current_row + 1 == row_count):
+            self.model().insertRow(row_count, root)
+        elif mod == Qt.ControlModifier:
+            if key == Qt.Key_Insert:
+                self.model().insertRow(current_row, root)
+            elif key == Qt.Key_Delete:
+                self.removeRow(current_row, root)
+        super(TableView, self).keyPressEvent(event)
+
+
+class TableView(QtGui.QTableView, EditableView):
+
+    pass
+
+
+class TreeView(QtGui.QTreeView, EditableView):
+
+    pass
