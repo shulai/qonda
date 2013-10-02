520e9a5e7b92f9fb705089c652b35c2d36e07e60
Julio César Gázquez <julio@mebamutual.com.ar>
2013-10-02 08:34:12 -0300

    Make setup.py compile resources automatically

--- setup.py
+++ setup.py
@@ -16,7 +16,42 @@
 # You should have received a copy of the GNU General Public License
 # along with Qonda; If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
 from setuptools import setup
+
+try: # Python 3.x
+    from setuptools.command.build_py import build_py_2to3 as build_py
+except ImportError: # Python 2.x
+    from setuptools.command.build_py import build_py
+import sys
+import subprocess
+
+class build(build_py):
+           
+    def _compile_ui(self):
+        print("Compiling Qt resource file")
+        self.pyrcc4 = 'pyrcc4'
+
+        if sys.version_info[0] == 2:
+            option = '-py2'
+        else:
+            option = '-py3'
+            
+        try:
+            subprocess.call([self.pyrcc4, option, 'icons/icons.qrc', '-o',
+                             'icons/icons_rc.py'])
+        except OSError:
+
+            print("rcc command failed - make sure that pyrcc4 "
+                  "or pyside-rcc4 is in your $PATH, or specify "
+                  "a custom command with --rcc=command")
+
+    def run(self):
+
+        self._compile_ui()
+        build_py.run(self)
+
+cmdclass = {'build_py':  build}
 
 setup(
     name='qonda',
@@ -24,7 +59,7 @@ setup(
     description="A Model-View framework based on Qt Interview",
     author="Julio Cesar Gazquez",
     author_email='julio@mebamutual.com.ar',
-    url='http://jotacege.com.ar/qonda',
+    url='https://bitbucket.org/shulai/qonda',
     packages=['qonda', 'qonda.mvc', 'qonda.widgets', 'qonda.mainwindow', 'qonda.util', 'qonda.sqlalchemy', 'qonda.icons'],
     package_dir={'qonda': '.'},
     use_2to3=True,
@@ -50,5 +85,6 @@ setup(
         "Topic :: Software Development :: User Interfaces",
         ],
     keywords='gui pyqt4',
-    license='GPL'
+    license='GPL',
+    cmdclass=cmdclass
         )
