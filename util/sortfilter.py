e0c7015bcbe1bcb48c698721bd17f9dc9c29183c
Julio Gazquez <julio@mebamutual.com.ar>
2014-09-23 21:26:38 -0300

    Adds SortFilterProxyModel proxy class

---
+++ util/sortfilter.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4.QtGui import QSortFilterProxyModel
+
+
+class SortFilterProxyModel(QSortFilterProxyModel):
+    """
+        A minimal extension to QSortFilterProxyModel implementing
+        Qonda's adapters API, notably getPyObject(), required by
+        currentPyObject() methods in DataWidgetMapper and *View widgets
+    """
+
+    def getPyModel(self):
+        return self.sourceModel.getPyModel()
+
+    def getPyObject(self, index):
+        return self.sourceModel().getPyObject(self.mapToSource(index))
+
+    def getPropertyColumn(self, propertyName):
+        return self.sourceModel().getPropertyColumn(propertyName)
+
+    def getColumnProperty(self, col):
+        return self.sourceModel().getColumnProperty(col)
+
+    def properties(self):
+        return self.sourceModel().properties()
