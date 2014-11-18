c9d284fefe2762aea2a32dee19edfef536b59b94
Julio César Gázquez <julio@mebamutual.com.ar>
2014-11-18 14:20:42 -0300

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ cmdclass = {'build_py': build}
 
 setup(
     name='qonda',
-    version='0.6.2',
+    version='0.6.3',
     description="A Model-View framework based on Qt Interview",
     author="Julio Cesar Gazquez",
     author_email='julio@mebamutual.com.ar',
